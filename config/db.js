240e26d2883758e524d0d564243592d2a8c1d7bd
Smartex_Dan <adeoyedan100@gmail.com>
2026-05-05 16:41:22 +0100

    Switch to PostgreSQL

--- config/db.js
+++ config/db.js
@@ -1,22 +1,13 @@
-const mysql = require('mysql2');
+const { Pool } = require('pg');
 require('dotenv').config();
 
-const db = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
-    ssl: process.env.DB_SSL ? { rejectUnauthorized: false } : false
+const db = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }
 });
 
-db.getConnection((err, connection) => {
-    if (err) {
-        console.error('Database connection failed:', err.message);
-        return;
-    }
-    console.log('MySQL Connected!');
-    connection.release();
-});
+db.connect()
+    .then(() => console.log('PostgreSQL Connected!'))
+    .catch(err => console.error('Database connection failed:', err.message));
 
-module.exports = db.promise();
+module.exports = db;
